06b9f2572598d9396c8080e51d35e0ce95b56271
知否 <vclinlin@vip.qq.com>
2018-10-08 20:37:06 +0800

    商品列表初建

---
+++ application/index/controller/Commodity.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17424
+ * Date: 2018/10/8
+ * Time: 11:27
+ */
+
+namespace app\index\controller;
+
+
+use app\index\model\Books;
+
+class Commodity extends Index
+{
+    public function Purchase($key=null)
+    {
+        $book = new Books();
+        if($key == null)
+        {
+            $data = $book->alias('A')
+                ->join('item_class B','A.item_class_id = B.Id','LEFT')
+                ->order('sales','desc')
+                ->paginate(15,'false',['query' =>request()->param()]);
+            $this->assign('book',$data);
+            $this->assign('page',$data->render());
+        }else{
+            $data = $book->alias('A')
+                ->join('item_class B','A.item_class_id = B.Id','LEFT')
+                ->where('A.bookname|B.name|B.book_class_name|A.press','like','%'.htmlentities($key).'%')
+                ->order('sales','desc')
+                ->paginate(15,'false',['query' =>request()->param()]);
+            $this->assign('book',$data);
+            $this->assign('page',$data->render());
+        }
+        return $this->fetch();
+    }
+}
---
+++ application/index/model/Books.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17424
+ * Date: 2018/10/8
+ * Time: 11:33
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class Books extends Model
+{
+
+}

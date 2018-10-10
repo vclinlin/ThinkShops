5be5b95f505535565b4f1583a8e77fa7e3c4e27b
知否 <vclinlin@vip.qq.com>
2018-10-10 20:59:31 +0800

    完善商品文本信息的编辑

--- application/index/controller/Commodity.php
+++ application/index/controller/Commodity.php
@@ -9,7 +9,9 @@
 namespace app\index\controller;
 
 
+use app\index\model\Book_class;
 use app\index\model\Books;
+use app\index\model\Item_class;
 
 class Commodity extends Index
 {
@@ -39,13 +41,28 @@ class Commodity extends Index
         return $this->fetch();
     }
 
+    public function getBookClass()//加载主分类
+    {
+        $model = new Book_class();
+        $data = $model->field('Id,name')->select();
+        echo json_encode($data);
+        return;
+    }
+
+    public function getItemClass($id)//加载二级分类
+    {
+        $model = new Item_class();
+        $data  = $model->where(['book_class_id'=>$id])->field('Id,name')->select();
+        echo json_encode($data);
+        return;
+    }
     public function setCover($Id)
     {
         $book = new Books();
         $data = $book->alias('A')
             ->join('item_class B','A.item_class_id = B.Id','LEFT')
             ->where(['A.Id'=>$Id])
-            ->field('A.*,B.book_class_name,B.name')
+            ->field('A.*,B.book_class_name,B.name,B.book_class_id')
             ->order('sales','desc')->find();
         //不存在的商品
         if(!$data)
@@ -55,5 +72,55 @@ class Commodity extends Index
         }
         $this->assign('book',$data);
         return $this->fetch();
+    }
+
+    public function editCommodity($Id)
+    {
+        $book = new Books();
+        if(!$rel = $book->get(['Id'=>$Id]))
+        {
+            echo json_encode([
+                'state'=>400,
+                'msg'=>'非正常操作'
+            ]);
+            return;
+        }
+        $data = $this->request->post();
+        if(isset($data['bookname']))
+        {
+            $data['bookname'] = htmlentities($data['bookname']);  //转义书籍名
+        }
+        if(isset($data['press']))
+        {
+            $data['press'] = htmlentities($data['press']);    //转义出版社名
+        }
+        if(isset($data['r_time']))
+        {
+            $data['r_time'] = strtotime($data['r_time']);     //转换为时间戳
+        }
+        if(isset($data['count']))
+        {
+            if($data['count']<$rel['sales'])  //总数小于销量
+            {
+                echo json_encode([
+                    'state'=>400,
+                    'msg'=>'该书籍已售'.$rel['sales'].'本,故最低数量不得低于'.$rel['sales'].'本'
+                ]);
+                return;
+            }
+        }
+        if(!$book->where(['Id'=>$Id])->update($data))
+        {
+            echo json_encode([
+                'state'=>400,
+                'msg'=>'信息更新失败'
+            ]);
+            return;
+        }
+        echo json_encode([
+            'state'=>200,
+            'msg'=>'信息更新完成'
+        ]);
+        return;
     }
 }
